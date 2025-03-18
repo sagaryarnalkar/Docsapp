327aaf1bd78247e1a8431ab03eb0aa7328b55c04
sagaryarnalkar <sagaryarnalkar@gmail.com>
2025-03-18 10:16:09 +0530

    Replace rag_processor.py with lightweight compatibility wrapper

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,5 @@
 from .user_state import UserState
 from .docs import DocsApp
+from .rag_processor import RAGProcessor, RAGProcessorError
 
-__all__ = ['UserState', 'DocsApp']
+__all__ = ['UserState', 'DocsApp', 'RAGProcessor', 'RAGProcessorError']
